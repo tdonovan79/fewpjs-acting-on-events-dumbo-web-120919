9707d877945ea71abc3780706eaa3b232ccb98d3
tdonovan79 <tdonovan79@gmail.com>
2020-01-27 21:12:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
